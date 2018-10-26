a2850757af29193fe8ea0ebd8f99220200e0923f
dzonib <nikolabosnjak381@gmail.com>
2018-10-26 22:05:55 +0200

    worked on styling for header and nav next global styles and typography

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
 import Meta from '../components/Meta'
-import styled from 'styled-components'
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components'
 
-const MyButon = styled.button`
-  background: red;
-  font-size: 100px;
+const theme = {
+	red: '#FF0000',
+	black: '#393939',
+	gray: '#3A3A3A',
+	lightGray: '#E1E1E1',
+	offWhite: '#EDEDED',
+	maxWidth: '1000px',
+	bs: '0 12px 24px 0 rgbqa(0, 0, 0, 0.09)'
+}
+
+const StyledPage = styled.div`
+	background: white;
+	color: ${props => props.theme.black};
+`
+
+const Inner = styled.div`
+	background: white;
+	max-width: ${props => props.theme.maxWidth};
+	margin: 0 auto;
+	padding: 2rem;
 `
 
 export default class Page extends Component {
 	render() {
 		return (
-			<div>
-				<Meta />
-				<Header />
-				<MyButon>Click me!</MyButon>
-				{this.props.children}
-			</div>
+			<ThemeProvider theme={theme}>
+				<StyledPage>
+					<Meta />
+					<Header />
+					<Inner>{this.props.children}</Inner>
+				</StyledPage>
+			</ThemeProvider>
 		)
 	}
 }
